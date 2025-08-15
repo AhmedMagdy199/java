f8f822ddedc3dd8044a2c734083671305af148ab
Ahmed Magdy <104032428+AhmedMagdy199@users.noreply.github.com>
2025-08-15 11:50:59 +0300

    Update BuildAndPushDocker.groovy

--- src/org/example/BuildAndPushDocker.groovy
+++ src/org/example/BuildAndPushDocker.groovy
@@ -1,22 +1,29 @@
 package org.example
 
-class BuildAndPushDocker implements Serializable {
+class DockerBuildPush implements Serializable {
     def script
 
-    BuildAndPushDocker(def script) {
+    DockerBuildPush(def script) {
         this.script = script
     }
 
+    /**
+     * Build and push a container image using Podman.
+     *
+     * @param dockerCredId - Jenkins credentials ID for container registry (username/password)
+     * @param imageName    - Full image name including registry (e.g., quay.io/user/app)
+     * @param version      - Image tag/version
+     */
     void run(String dockerCredId, String imageName, String version) {
         script.withCredentials([script.usernamePassword(
-            credentialsId: dockerCredId, 
-            usernameVariable: 'DOCKER_USER', 
+            credentialsId: dockerCredId,
+            usernameVariable: 'DOCKER_USER',
             passwordVariable: 'DOCKER_PASSWORD'
         )]) {
             script.sh """
-                echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USER" --password-stdin
-                docker build -t ${imageName}:${version} .
-                docker push ${imageName}:${version}
+                echo "\$DOCKER_PASSWORD" | podman login -u "\$DOCKER_USER" --password-stdin
+                podman build -t ${imageName}:${version} .
+                podman push ${imageName}:${version}
             """
         }
     }
