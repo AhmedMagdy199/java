4c7be45f8eed1890be41187ded7d035c6eab7f56
Ahmed Magdy <104032428+AhmedMagdy199@users.noreply.github.com>
2025-08-15 12:21:58 +0300

    Update BuildAndPushDocker.groovy

--- src/org/example/BuildAndPushDocker.groovy
+++ src/org/example/BuildAndPushDocker.groovy
@@ -1,30 +1,30 @@
 package org.example
 
 class DockerBuildPush implements Serializable {
-    def script
+    def script
 
-    DockerBuildPush(def script) {
-        this.script = script
-    }
+    DockerBuildPush(def script) {
+        this.script = script
+    }
 
-    /**
-     * Build and push a container image using Docker.
-     *
-     * @param dockerCredId - Jenkins credentials ID for container registry (username/password)
-     * @param imageName    - Full image name including registry (e.g., quay.io/user/app)
-     * @param version      - Image tag/version
-     */
-    void run(String dockerCredId, String imageName, String version) {
-        script.withCredentials([script.usernamePassword(
-            credentialsId: dockerCredId,
-            usernameVariable: 'DOCKER_USER',
-            passwordVariable: 'DOCKER_PASSWORD'
-        )]) {
-            script.sh """
-                echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USER" --password-stdin
-                docker build -t ${imageName}:${version} .
-                docker push ${imageName}:${version}
-            """
-        }
-    }
+    /**
+     * Build and push a container image using Docker.
+     *
+     * @param dockerCredId - Jenkins credentials ID for container registry (username/password)
+     * @param imageName    - Full image name including registry (e.g., quay.io/user/app)
+     * @param version      - Image tag/version
+     */
+    void run(String dockerCredId, String imageName, String version) {
+        script.withCredentials([script.usernamePassword(
+            credentialsId: dockerCredId,
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASSWORD'
+        )]) {
+            script.sh """
+                echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USER" --password-stdin
+                docker build -t ${imageName}:${version} .
+                docker push ${imageName}:${version}
+            """
+        }
+    }
 }
