bbe953296a2f0904eec43aefd6bb4877e73e2300
Ahmed Magdy <104032428+AhmedMagdy199@users.noreply.github.com>
2025-08-16 20:44:29 +0300

    Update SonarQube.groovy

--- src/org/example/SonarQube.groovy
+++ src/org/example/SonarQube.groovy
@@ -8,12 +8,16 @@ class SonarQube implements Serializable {
     void run(String projectKey, String projectName, String sonarTokenCredentialId) {
         script.withCredentials([script.string(credentialsId: sonarTokenCredentialId, variable: 'SONAR_TOKEN')]) {
             script.sh """
+                # Ensure using Java 17 from Maven container
+                export JAVA_HOME=/opt/java/openjdk
+                export PATH=\$JAVA_HOME/bin:\$PATH
+
                 sonar-scanner \\
-                -Dsonar.projectKey=${projectKey} \\
-                -Dsonar.projectName="${projectName}" \\
-                -Dsonar.sources=. \\
-                -Dsonar.host.url=http://192.168.1.22:31000 \\
-                -Dsonar.login=\$SONAR_TOKEN
+                  -Dsonar.projectKey=${projectKey} \\
+                  -Dsonar.projectName="${projectName}" \\
+                  -Dsonar.sources=. \\
+                  -Dsonar.host.url=http://192.168.1.22:31000 \\
+                  -Dsonar.login=\$SONAR_TOKEN
             """
         }
     }
