5bc929ce8cf307ac3bcc27822ee3dc996ee5c783
Ahmed Magdy <104032428+AhmedMagdy199@users.noreply.github.com>
2025-08-17 02:10:41 +0300

    Update BuildAndPushDocker.groovy

--- src/org/example/BuildAndPushDocker.groovy
+++ src/org/example/BuildAndPushDocker.groovy
@@ -8,7 +8,7 @@ class DockerBuildPush implements Serializable {
     }
 
     /**
-     * Build and push a container image using Podman inside Kubernetes pod.
+     * Build and push a container image using Podman inside a Kubernetes pod.
      *
      * @param dockerCredId - Jenkins credentials ID for container registry (username/password)
      * @param imageName    - Full image name including registry (e.g., docker.io/user/app)
@@ -20,11 +20,12 @@ class DockerBuildPush implements Serializable {
             usernameVariable: 'REG_USER',
             passwordVariable: 'REG_PASS'
         )]) {
-            script.sh """
-                echo "\$REG_PASS" | podman login -u "\$REG_USER" --password-stdin $(echo ${imageName} | awk -F/ '{print \$1}')
-                podman build -t ${imageName}:${version} .
-                podman push ${imageName}:${version}
-            """
+            script.sh '''
+                REGISTRY=$(echo ''' + imageName + ''' | awk -F/ '{print $1}')
+                echo "$REG_PASS" | podman login -u "$REG_USER" --password-stdin $REGISTRY
+                podman build -t ''' + imageName + ''':''' + version + ''' .
+                podman push ''' + imageName + ''':''' + version + '''
+            '''
         }
     }
 }
