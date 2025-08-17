08543ea226909b40b1936605248fdcfefba98e21
Ahmed Magdy <104032428+AhmedMagdy199@users.noreply.github.com>
2025-08-17 04:04:08 +0300

    Update SlackNotifier.groovy

--- src/org/example/SlackNotifier.groovy
+++ src/org/example/SlackNotifier.groovy
@@ -3,14 +3,14 @@ package org.example
 class SlackNotifier implements Serializable {
     def script
 
-    SlackNotifier(def script) { this.script = script }
+    SlackNotifier(def script) {
+        this.script = script
+    }
 
-    void notify(String message, String slackToken) {
-        script.withCredentials([script.string(credentialsId: slackToken, variable: 'SLACK_TOKEN')]) {
+    void notify(String message, String slackCredsId) {
+        script.withCredentials([script.string(credentialsId: slackCredsId, variable: 'SLACK_WEBHOOK_URL')]) {
             script.sh """
-                curl -X POST -H 'Authorization: Bearer \$SLACK_TOKEN' -H 'Content-type: application/json' \
-                --data '{"channel":"#general","text":"${message}"}' \
-                https://slack.com/api/chat.postMessage
+                curl -X POST -H 'Content-type: application/json' --data '{"text":"${message}"}' \$SLACK_WEBHOOK_URL
             """
         }
     }
